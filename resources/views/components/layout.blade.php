93fbb3b6c034d89643744c0952216013e766201f
Rahulkoju <blcraz75@gmail.com>
2024-10-26 16:17:35 +0545

    add: multiple blade components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,20 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JobSearch</title>
+    @vite(['resources/js/app.js','resources/css/app.css'])
 </head>
 
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="/">
                     <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="" srcset="">
                 </a>
             </div>
-            <div>links</div>
-            <div>post a job</div>
+            <div class="space-x-6 font-bold">
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
         </nav>
-        <main>
+        <main class="mt-10 m-x-[986px]">
             {{ $slot }}
         </main>
     </div>
