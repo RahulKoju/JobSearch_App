18f8f0ecf930aa2c684efa7a3d8bc34f63097d10
Rahulkoju <blcraz75@gmail.com>
2024-10-29 23:02:30 +0545

    add: logout functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,8 +26,13 @@
                 <a href="">Companies</a>
             </div>
             @auth
-            <div>
+            <div class="space-x-6 font-bold flex">
                 <a href="/jobs/create">Post a Job</a>
+                <form method="POST" action="/logout">
+                    @csrf
+                    @method('DELETE')
+                    <button>Log Out</button>
+                </form>
             </div>
             @endauth
 
