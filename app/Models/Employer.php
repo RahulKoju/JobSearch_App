52d41cf63d9e4c71b8f538ff6ccae21da25907c6
Rahulkoju <blcraz75@gmail.com>
2024-10-30 13:43:40 +0545

    fix: employer logo loading issue by using Laravel's Attribute Accessors feature

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Storage;
 
 class Employer extends Model
 {
@@ -18,5 +20,10 @@ class Employer extends Model
     public function jobs(): HasMany
     {
         return $this->hasMany(Job::class);
+    }
+    // In Employer.php (Model)
+    public function getLogoUrlAttribute()
+    {
+        return Str::startsWith($this->logo, 'http') ? $this->logo : Storage::url($this->logo);
     }
 }
